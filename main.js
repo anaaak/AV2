539bd6c771806cbbfafc3e9905acb2682c795476
ana ketllyn <105050863+anaaak@users.noreply.github.com>
2023-11-09 08:21:37 -0300

    dale

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function tocaSom(idElementoAudio){
 document.querySelector(idElementoAudio).play();
 }
-const listaDeTeclas =  document.querySelectorAll("tecla");
+const listaDeTeclas =  document.querySelectorAll(".tecla");
 
 let contador = 0;
-while(contador<listaDeTeclas.length){
-    const efeito - listaDeTeclas[contador].classList[1];
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
     const idAudio = "#som_"+efeito;
     listaDeTeclas[contador].onclick = function(){
         tocaSom(idAudio);
